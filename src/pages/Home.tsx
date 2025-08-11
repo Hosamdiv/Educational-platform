daaf4b1ba09d6363efeca543b3620711528678d5
Hosam Reda <rdhosam712@gmail.com>
2025-08-11 04:13:30 +0300

    update new 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
+import BestSeller from "../components/BestSeller"
+import CurPolicy from "../components/CurPolicy"
 import Hero from "../components/Hero"
 import LatestCollection from "../components/LatestCollection"
+import NewsletterBox from "../components/NewsletterBox"
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
+      <CurPolicy />
+      <NewsletterBox />
     </div>
   )
 }
