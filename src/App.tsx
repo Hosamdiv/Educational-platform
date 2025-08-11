3ccc4f06ab9484a144a80320bd1e260c8039eec8
Hosam Reda <rdhosam712@gmail.com>
2025-08-11 04:36:56 +0300

    update new 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router";
 import router from "./routes";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <div className="px-4 sm:px-[5vw] md:px-[7vm] lg:px-[9vm]">
         <RouterProvider router={router} />
+        <Footer />
       </div>
     </>
   );
